7b79261d058d90aefb904cfac0a75ea8004b9ef1
xiaohaoxing <xiaohaoxing@outlook.com>
2023-03-30 17:43:34 +0800

    feat: 添加 JNI 支持

--- LSM.h
+++ LSM.h
@@ -13,7 +13,7 @@
 class LSM {
     public:
         LSM(std::map<std::string, std::string> merge_maps,
-            std::vector<std::vector<std::string>> keys_per_level,
+            std::vector<std::vector<std::string> > keys_per_level,
             std::vector<unsigned int> element_size_threshold_per_level,
             std::vector<unsigned int> element_length_per_level,
             std::vector<unsigned int> runs_per_level);
@@ -28,7 +28,6 @@ class LSM {
 
         void insert_kv(const std::string& k, std::string v);
 
-
         // 暂时不考虑删除
         void delete_kv(std::string k, std::string v);
 
@@ -38,14 +37,12 @@ class LSM {
         std::vector<DiskLevel *> _disk_levels;
         MemLevel* _mem_level;
         std::map<std::string, std::string> _merge_maps;  // 记录上一层中的run与下一层哪一个run合并
-        std::vector<std::vector<std::string>> _keys_per_level;   // 记录每一层的run对应的key值
+        std::vector<std::vector<std::string> > _keys_per_level;   // 记录每一层的run对应的key值
         std::vector<unsigned int> _element_size_threshold_per_level;  // 记录每一层的run对应的元素数量阈值
         std::vector<unsigned int> _element_length_per_level;   // 记录每一层的元素长度
         std::vector<unsigned int> _runs_per_level;   //  记录每一层run的数量
         // 所有的run的element_size设为相同
         std::map<std::string, MemRUN *> _memruns_for_key; //记录每个key对应的 MemRUN，插入k,v时用
-
-        // 不考虑删除功能
 
 };
 
