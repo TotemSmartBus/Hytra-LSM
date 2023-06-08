c9fc76a1d25f78823a3e8dfdffc1be055b446f18
riaaa <466874927@qq.com>
2023-06-08 21:15:01 +0800

    fix: diskrun test passed

--- FileMapper.cpp
+++ FileMapper.cpp
@@ -18,6 +18,7 @@ FileMapper::FileMapper(std::string file_name,int element_size_threshold,int elem
     _cur_size = 0;
     open_file();
     ftruncate(_fd,file_size);
+    int len = lseek(_fd, 0, SEEK_END);
     close_file();
 }
 
@@ -43,25 +44,11 @@ std::string FileMapper::read_element(unsigned int ele_index){
 
 }
 
-
-// 多次单个元素写入效率较低，弃用
-void FileMapper::write_element(std::string element){
-    open_file();
-    do_map();
-    size_t offset = _cur_size*_element_length;
-    const char* ele = element.c_str();
-    memcpy(_fmap+offset,ele,_element_length);
-    _cur_size++;
-    un_map();
-    close_file();
-}
-
 // 删除文件并将文件内的元素数量置为0
 void FileMapper::clear(){
     remove(("index_file/"+_file_name).c_str());
     _cur_size = 0;
 }
-
 
 // 按批次读取文件，加快读取速度
 std::vector<std::string> FileMapper::read_all_element(){
@@ -71,7 +58,7 @@ std::vector<std::string> FileMapper::read_all_element(){
     }
 
     open_file();
-    ftruncate(_fd,file_size);
+    ftruncate(_fd,file_size); // 读取文件为什么还分配大小？？？
     do_map();
     size_t offset=0;
     char* val = new char[_element_length];
@@ -96,7 +83,6 @@ void FileMapper::expand_size(){
     _element_size_threshold = _cur_size*2;
 }
 
-
 void FileMapper::adjust_size(unsigned int new_size) {
     open_file();
     file_size = new_size;
@@ -106,10 +92,15 @@ void FileMapper::adjust_size(unsigned int new_size) {
 }
 
 void FileMapper::do_map() {
+    // 获得映射区的首地址
     _fmap = (char*) mmap(NULL, file_size,PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0);
 }
 
 void FileMapper::un_map() {
+//    int ret = munmap(_fmap,file_size); // 释放内存映射
+//    if(ret==-1){
+//        perror("munmap error\n");
+//    }
     fsync(_fd);
     if(munmap(_fmap,file_size)==-1){
         perror("unmap error\n");
@@ -117,7 +108,11 @@ void FileMapper::un_map() {
 }
 
 void FileMapper::open_file() {
-    _fd = open(("index_file/"+_file_name).c_str(),O_RDWR | O_CREAT | O_TRUNC ,(mode_t) 0600);
+
+//    _fd = open(("index_file/"+_file_name).c_str(),O_RDWR | O_CREAT | O_TRUNC ,(mode_t) 0600);
+    // 2023.06.08 使用 O_TRUNC 时，每次打开文件文件内容会清空
+//    _fd = open(("index_file/"+_file_name).c_str(),O_RDWR | O_CREAT | O_TRUNC ,S_IRWXU|S_IRWXG);
+    _fd = open(("index_file/"+_file_name).c_str(),O_RDWR | O_CREAT,S_IRWXU|S_IRWXG);
     if(_fd == -1){
         perror(("Create file failed " + _file_name).c_str());
     }
@@ -127,18 +122,60 @@ void FileMapper::close_file() {
     close(_fd);
 }
 
-// 按批次进行写入，提高写入效率
-void FileMapper::write_batch(std::set<std::string> values) {
+// 多次单个元素写入效率较低，弃用
+void FileMapper::write_element(std::string element){
     open_file();
-    ftruncate(_fd, file_size);
     do_map();
-    size_t offset;
-    for(const auto& s:values){
-        offset = _cur_size * _element_length;
-        const char* tmp = s.c_str();
-        memcpy(_fmap+offset, tmp, _element_length);
-        _cur_size++;
-    }
+    size_t offset = _cur_size*_element_length;
+    const char* ele = element.c_str();
+    memcpy(_fmap+offset,ele,_element_length);
+    _cur_size++;
     un_map();
     close_file();
+}
+
+// 按批次进行写入，提高写入效率
+void FileMapper::write_batch(std::set<std::string> values) {
+//    open_file(); // 打开文件，并生成文件标识符 _fd；mmap创建映射区完成后即可关闭，后续访问文件用指针/地址
+//    ftruncate(_fd, file_size); // 扩展文件大小
+//    do_map(); // mmap
+//    size_t offset;
+//    for(const auto& s:values){
+//        offset = _cur_size * _element_length;
+//        const char* tmp = s.c_str();
+//        memcpy(_fmap, tmp, sizeof(tmp));
+//        _cur_size++;
+//    }
+//    un_map();
+//    close_file();
+
+    open_file(); // 打开文件，并生成文件标识符 _fd；mmap创建映射区完成后即可关闭，后续访问文件用指针/地址
+    ftruncate(_fd, file_size); // 扩展文件大小
+
+    int len = lseek(_fd, 0, SEEK_END); // 获取文件大小，作为 mmap() 的第二个参数
+    printf("%d   ", len);
+
+    do_map(); // mmap
+
+    if(_fmap == MAP_FAILED) // 出错判断
+    {
+        perror("mmap error");
+        exit(1);
+    }
+//    close_file();
+
+    int offset = 0;
+    for(std::string s:values){
+        offset = _cur_size * _element_length;
+        const char* tmp = s.c_str();
+        memcpy(_fmap+offset, tmp, strlen(tmp));
+        _cur_size++;
+    }
+
+//    printf("%s   ", _fmap); // 用首地址获取这块区域的内容
+//    printf("%s   ", _fmap+_element_length);
+
+    un_map();
+    close_file();
+
 }
--- test_filemapper_put_and_get.cpp
+++ test_filemapper_put_and_get.cpp
@@ -15,12 +15,14 @@ int main() {
     std::string key = "2023";
     unsigned int _level = 0;
     unsigned int _run_size_threshold =  3;
+
     DiskRUN* diskrun = new DiskRUN(_run_element_length,key,_level,_run_size_threshold);
 
     std::set<std::string> s;
-    s.insert("1");
-    s.insert("2");
-    s.insert("3");
+    s.insert("22");
+    s.insert("33");
+    s.insert("44");
+
     diskrun->add_batch(s);
 
     std::vector<std::string> result = diskrun->get_all_elements();
